c1d1304f378e4a636b721c01221e67626c10036d
Triantafyllos Efraim Kiosses <p3210079@aueb.gr>
2024-01-04 19:50:18 +0200

    Owner view listing

--- app/src/main/java/com/example/readysethome/view/Owner/OwnerViewListing/OwnerViewListingActivity.java
+++ app/src/main/java/com/example/readysethome/view/Owner/OwnerViewListing/OwnerViewListingActivity.java
@@ -1,5 +1,6 @@
 package com.example.readysethome.view.Owner.OwnerViewListing;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -9,21 +10,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.readysethome.R;
-import com.example.readysethome.dao.ListingDAO;
 import com.example.readysethome.memorydao.ListingDAOMemory;
 
 public class OwnerViewListingActivity extends AppCompatActivity implements OwnerViewListingView{
 
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_viewlisting);
 
         Intent intent = getIntent();
-        String listing_id = intent.getStringExtra("LISTING_ID");
-        final OwnerViewListingPresenter presenter = new OwnerViewListingPresenter(new ListingDAOMemory(), Integer.parseInt(listing_id));
-
+        int listing_id = intent.getIntExtra("LISTING_ID", 0);
+        OwnerViewListingPresenter presenter = new OwnerViewListingPresenter(new ListingDAOMemory(), listing_id);
+        presenter.setUpInfo();
         String title = getIntent().getStringExtra("TITLE");
         String desc = getIntent().getStringExtra("DESCRIPTION");
         String price = getIntent().getStringExtra("PRICE");
@@ -33,5 +33,12 @@ public class OwnerViewListingActivity extends AppCompatActivity implements Owner
         ((TextView)findViewById(R.id.owner_viewListing_desc)).setText(desc);
         ((TextView)findViewById(R.id.owner_viewListing_price)).setText(price);
         ((ImageView)findViewById(R.id.owner_viewListing_image)).setImageResource(image_rsc);
+
+
+        ((TextView)findViewById(R.id.owner_viewListing_incomePerMonthValue)).setText(Double.toString(presenter.getMonthlyIncome()));
+        ((TextView)findViewById(R.id.owner_viewListing_bookingsPerMonthValue)).setText(Double.toString(presenter.getMonthlyBookings()));
+        ((TextView)findViewById(R.id.owner_viewListing_cancelationsPerMonthValue)).setText(Double.toString(presenter.getMonthlyCancelations()));
+        ((TextView)findViewById(R.id.owner_viewListing_ratingValue)).setText(Double.toString(presenter.getListing().getRating()));
+
     }
 }
--- app/src/main/java/com/example/readysethome/view/Owner/OwnerViewListing/OwnerViewListingPresenter.java
+++ app/src/main/java/com/example/readysethome/view/Owner/OwnerViewListing/OwnerViewListingPresenter.java
@@ -1,23 +1,66 @@
 package com.example.readysethome.view.Owner.OwnerViewListing;
 
 
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.View;
 
 import com.example.readysethome.R;
 import com.example.readysethome.dao.ListingDAO;
 import com.example.readysethome.model.Listing;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class OwnerViewListingPresenter {
     private Listing listing;
+    private double monthlyIncome;
+    private int monthlyCancelations;
+    private double monthlyBookings;
 
     public OwnerViewListingPresenter(ListingDAO listing, int id) {
         this.listing = listing.findByID(id);
     }
 
+    public OwnerViewListingPresenter(OwnerViewListingPresenter presenter) {
+        this.listing = presenter.listing;
+        this.monthlyIncome = presenter.monthlyIncome;
+        this.monthlyCancelations = presenter.monthlyCancelations;
+        this.monthlyBookings = presenter.monthlyBookings;
+    }
+
+    public Listing getListing() {
+        return listing;
+    }
+
+    public double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public int getMonthlyCancelations() {
+        return monthlyCancelations;
+    }
+
+    public double getMonthlyBookings() {
+        return monthlyBookings;
+    }
+
     public void setUpInfo() {
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm");
+        String yearmonth = dateFormat.format(date);
 
+        listing.calculateMonthlyIncome(date);
+        monthlyIncome = listing.getMonthlyIncome().get(yearmonth);
+//        ((TextView) view.findViewById(R.id.owner_viewListing_incomePerMonthValue)).setText(String.valueOf(monthlyIncome));
 
+        listing.calculateCancellationsPerMonth(date);
+        monthlyCancelations = listing.getMonthlyCancellations().get(yearmonth);
+//        ((TextView) view.findViewById(R.id.owner_viewListing_cancelationsPerMonthValue)).setText(String.valueOf(monthlyCancelations));
+
+        java.util.Calendar cal = java.util.Calendar.getInstance();
+        monthlyBookings = listing.calculateOccupancy(cal);
+//        ((TextView) view.findViewById(R.id.owner_viewListing_bookingsPerMonthValue)).setText(String.valueOf(monthlyBookings));
     }
 }
