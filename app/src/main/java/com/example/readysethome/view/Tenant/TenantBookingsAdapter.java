359e868ec28db28470c63d57289d2d93f52bbb1f
anestis_th <p3210273@aueb.gr>
2024-01-06 15:45:27 +0200

    Recycle view /Gui for bookings and booking request on tenant bookingafragment + Additions to tenant daos

---
+++ app/src/main/java/com/example/readysethome/view/Tenant/TenantBookingsAdapter.java
@@ -0,0 +1,69 @@
+package com.example.readysethome.view.Tenant;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.readysethome.R;
+
+import java.util.ArrayList;
+
+public class TenantBookingsAdapter extends RecyclerView.Adapter<TenantBookingsAdapter.MyViewHolder> {
+    TenantBookingsFragment fragment;
+    private Context context;
+    private ArrayList<TenantBookingModel> bookingModels;
+
+    public TenantBookingsAdapter(Context context, ArrayList<TenantBookingModel> bookingModels,TenantBookingsFragment fragment) {
+        this.context = context;
+        this.bookingModels = bookingModels;
+        this.fragment=fragment;
+    }
+
+    public void setBookingModels(ArrayList<TenantBookingModel> bookingModels) {
+        this.bookingModels = bookingModels;
+    }
+
+    @NonNull
+    @Override
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_view_tenant_booking, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        TenantBookingModel bookingModel = bookingModels.get(position);
+        holder.tvTitle.setText(bookingModel.getTitle());
+        holder.tvDate.setText(bookingModel.getDate());
+        holder.tvStatus.setText(bookingModel.getStatus());
+        holder.tvImage.setImageResource(bookingModels.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookingModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView tvImage;
+
+        TextView tvTitle, tvDate, tvStatus;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.recycler_view_tenant_booking_title);
+            tvDate = itemView.findViewById(R.id.recycler_view_tenant_booking_date);
+            tvStatus = itemView.findViewById(R.id.recycler_view_tenant_booking_status);
+            tvImage=itemView.findViewById(R.id.recylcler_view_tenant_booking_image);
+        }
+    }
+}
--- app/src/main/java/com/example/readysethome/view/Tenant/TenantBookingsFragment.java
+++ app/src/main/java/com/example/readysethome/view/Tenant/TenantBookingsFragment.java
@@ -1,66 +1,56 @@
 package com.example.readysethome.view.Tenant;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.readysethome.R;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TenantBookingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.readysethome.R;
+import com.example.readysethome.dao.TenantDAO;
+import com.example.readysethome.memorydao.TenantDAOMemory;
+import com.example.readysethome.model.Tenant;
+import com.example.readysethome.view.Tenant.TenantMain.TenantMainPresenter;
+
+import java.util.ArrayList;
+
 public class TenantBookingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private TenantBookingsAdapter adapter;
+    private TenantDAO tenantDAO;
+    private Tenant tenant;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TenantMainPresenter presenter;
 
-    public TenantBookingsFragment() {
-        // Required empty public constructor
+
+    private boolean init_recycle_view;
+
+    public TenantBookingsFragment(TenantMainPresenter presenter) {
+        this.presenter = presenter;
+        tenant = presenter.getAttachedTenant();
+        init_recycle_view=false;
+
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TenantBookingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TenantBookingsFragment newInstance(String param1, String param2) {
-        TenantBookingsFragment fragment = new TenantBookingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tenant_bookings, container, false);
+        View view = inflater.inflate(R.layout.fragment_tenant_bookings, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+
+
+        if(!init_recycle_view){
+            // Get all bookings and requests for the tenant using the presenter method
+            ArrayList<TenantBookingModel> bookingsAndRequests = presenter.setBookingModels();
+            adapter = new TenantBookingsAdapter(getContext(), bookingsAndRequests,this);
+            init_recycle_view=true;
+        }
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
     }
 }
